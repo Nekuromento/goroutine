9f6dd318391b61ed81195040fbaf936e4e8dd649
Max Klyga <max@getstream.io>
2017-11-20 17:33:20 +0100

    Drop support for versions earlier then 1.8

--- versions.go
+++ versions.go
@@ -15,21 +15,6 @@ type _GoVersion int
 const (
 	_GO_VERSION_INVALID _GoVersion = iota
 
-	_GO_VERSION1_5
-	_GO_VERSION1_5_1
-	_GO_VERSION1_5_2
-	_GO_VERSION1_5_3
-	_GO_VERSION1_5_4
-	_GO_VERSION1_6
-	_GO_VERSION1_6_1
-	_GO_VERSION1_6_2
-	_GO_VERSION1_6_3
-	_GO_VERSION1_7
-	_GO_VERSION1_7_1
-	_GO_VERSION1_7_2
-	_GO_VERSION1_7_3
-	_GO_VERSION1_7_4
-	_GO_VERSION1_7_5
 	_GO_VERSION1_8
 	_GO_VERSION1_8_1
 	_GO_VERSION1_8_2
@@ -51,36 +36,6 @@ func init() {
 
 	if err != nil {
 		return
-	} else if _goVersion.Equal("1", "5") {
-		_goVersionCode = _GO_VERSION1_5
-	} else if _goVersion.Equal("1", "5", "1") {
-		_goVersionCode = _GO_VERSION1_5_1
-	} else if _goVersion.Equal("1", "5", "2") {
-		_goVersionCode = _GO_VERSION1_5_2
-	} else if _goVersion.Equal("1", "5", "3") {
-		_goVersionCode = _GO_VERSION1_5_3
-	} else if _goVersion.Equal("1", "5", "4") {
-		_goVersionCode = _GO_VERSION1_5_4
-	} else if _goVersion.Equal("1", "6") {
-		_goVersionCode = _GO_VERSION1_6
-	} else if _goVersion.Equal("1", "6", "1") {
-		_goVersionCode = _GO_VERSION1_6_1
-	} else if _goVersion.Equal("1", "6", "2") {
-		_goVersionCode = _GO_VERSION1_6_2
-	} else if _goVersion.Equal("1", "6", "3") {
-		_goVersionCode = _GO_VERSION1_6_3
-	} else if _goVersion.Equal("1", "7") {
-		_goVersionCode = _GO_VERSION1_7
-	} else if _goVersion.Equal("1", "7", "1") {
-		_goVersionCode = _GO_VERSION1_7_1
-	} else if _goVersion.Equal("1", "7", "2") {
-		_goVersionCode = _GO_VERSION1_7_2
-	} else if _goVersion.Equal("1", "7", "3") {
-		_goVersionCode = _GO_VERSION1_7_3
-	} else if _goVersion.Equal("1", "7", "4") {
-		_goVersionCode = _GO_VERSION1_7_4
-	} else if _goVersion.Equal("1", "7", "5") {
-		_goVersionCode = _GO_VERSION1_7_5
 	} else if _goVersion.Equal("1", "8") {
 		_goVersionCode = _GO_VERSION1_8
 	} else if _goVersion.Equal("1", "8", "1") {
